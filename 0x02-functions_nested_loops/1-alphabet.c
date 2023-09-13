429a0cd101baa42fe8ac169d31bc270f23e83622
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-09-13 12:54:59 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point 
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		_putchar(x);
+	_putchar('\n');
+}
+
+
