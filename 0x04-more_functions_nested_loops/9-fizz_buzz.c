39871c4791fe57b9b82a9ffd963e1bcdbcc16117
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-09-15 14:22:40 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,28 @@ int main(void)
 
 	for (x = 1 ; x <= 100 ; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
-			printf("%s" . "FizzBuzz");
-		else if (x % 3 == 0)
-			printf("%s" . "Fizz");
-		else if (x % 5 == 0)
-			printf("%s" . "Buzz");
+		if (x % 3 == 0 && x % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0 && x % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (x == 1)
+		{
+			printf("%d", x);
+		}
 		else
-			printf("%d" . x);
-		if (x != 100)
-			printf(" ");
+		{
+			printf(" %d", x);
+		}
 	}
-	printf('\n');
+	printf("\n");
+
 	return (0);
 }
