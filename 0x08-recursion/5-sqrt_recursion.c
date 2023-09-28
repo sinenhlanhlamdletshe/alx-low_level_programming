48c43e2f8cbd6579a35a70ffb1c02506a08c4a12
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-09-28 14:30:06 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,5 +9,20 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_recursion(n, 0));
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find natural square root of a number
+ * @n: number to be squared
+ * @x: iterator
+ * Return: square root
+ */
+int actual_sqrt_recursion(int n, int x)
+{
+	if (x * x > n)
+		return (-1);
+	if (x * x == n)
+		return (x);
+	return (actual_sqrt_recursion(n, x + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int main(void);
+int actual_sqrt_recursion(int n, int x);
+
 #include <stdio.h>
 #endif
