991031250b6321f132ef3c1b10969ae429f38dba
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-10-05 15:34:31 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,39 +9,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int x;
-	unsigned int s1length = 0;
-	unsigned int s2length = 0;
 	char *mem;
+	unsigned int x = 0, y = 0, slength1 = 0, slength2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (x = 0 ; s1[x] != '\0' ; x++)
-		s1length++;
-	for (x = 0 ; s2[x] != '\0' ; x++)
-		s2length++;
+	while (s1 && s1[slength1])
+		slength1++;
+	while (s2 && s2[slength2])
+		slength2++;
 
-	mem = malloc(sizeof(char) * (s1length + n) + 1);
-	if (mem == NULL)
-		return (NULL);
-	if (n >= s2length)
-	{
-		for (x = 0 ; s1length[x] != '\0' ; x++)
-			mem[x] = s1[x];
-		for (x = 0 ; s2length[x] != '\0' ; x++)
-			mem[s1length + x] = s2[x];
-		mem[s1length + 1] = '\0';
-	}
+	if (n < slength2)
+		mem = malloc(sizeof(char) * (slength1 + n + 1));
 	else
+		mem = malloc(sizeof(char) * (slength1 + slength2 + 1));
+
+	if (!mem)
+		return (NULL);
+
+	while (x < slength1)
 	{
-		for (x = 0 ; s1[x] != '\0' ; x++)
-			mem[x] = s1[x];
-		for (x = 0 ; x < n ; x++)
-			mem[s1length + x] = s2[x];
-		mem[s1length + x] = '\0';
+		mem[x] = s1[x];
+		x++;
 	}
+
+	while (n < slength2 && x < (slength1 + n))
+		mem[x++] = s2[y++];
+
+	while (n >= slength2 && x < (slength1 + slength2))
+		mem[x++] = s2[y++];
+
+	mem[x] = '\0';
+
 	return (mem);
 }
-
