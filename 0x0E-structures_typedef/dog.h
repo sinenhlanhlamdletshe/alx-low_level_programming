b3f5d84361ba85651630aae499626d43c31abf7d
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-10-10 13:41:35 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information instructor
+ * @name: dog name
+ * @age: dog age
+ * @owner: detail of dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
