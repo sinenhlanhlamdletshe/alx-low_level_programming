1590a39204e97bf0260007cef438dec557828c8f
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-10-24 16:45:58 -0700

    Print (safe version)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a listint_t list
+ * @head: pointer to first element of the list
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
