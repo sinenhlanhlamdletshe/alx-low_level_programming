cadeb9a748f90aa9e18304f1da79d7438f44e3ea
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-10-24 01:34:52 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: head pointer to the list to print
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t digit = 0;
 
 	while (h)
 	{
-		num++;
+		digit++;
 		h = h->next;
 	}
-	return (num);
+	return (digit);
 }
 
