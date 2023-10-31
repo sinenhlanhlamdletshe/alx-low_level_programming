cb550a2996a1dc9afe6d172c401eae7b6c7a8ce6
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-10-31 15:48:04 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * create_file - creates a file
  * @filename: filename
  * @text_content: text written in the file
@@ -13,15 +13,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	x = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
+	x = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (x == -1)
 		return (-1);
 
-	if (!text_conten)
-		text_content= "";
+	if (!text_content)
+		text_content = "";
 
-	for (words = 0; text_content[words]; words++);
+	for (words = 0; text_content[words]; words++)
+		;
 
 	new = write(x, text_content, words);
 
