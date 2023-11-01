ab622ff830de7aea4a90fd267843bedc4e9a1b54
sinenhlanhlamdletshe <sne.mdletshe75@gmail.com>
2023-11-01 12:36:08 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,31 @@
 #include "main.h"
 
-/*
+/**
  * append_text_to_file - appends text at the end of file
- * @filename: filename
+ * @filename: name of the file
  * @text_content: content to be appended
  * Return: 1 (success) or -1 (failure)
  */
 int append_text_to_file(const char *filename, char *text_content)
-{       
+{
 	int x, new, words;
 
-        if (!filename)
-                return (-1);
-
-        x = open(filename, O_WRONLY | O_APPEND);
-
-        if (x == -1)
-                return (-1);
-
-        if (text_content)
-	{
-
-	for (words = 0; text_content[words]; words++);
-
-	new = write(x, text_content, words);
-
-	if (new == -1)
+	if (!filename)
 		return (-1);
+
+	x = open(filename, O_WRONLY | O_APPEND);
+
+	if (x == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (words = 0; text_content[words]; words++)
+			;
+		new = write(x, text_content, words);
+
+		if (new == -1)
+			return (-1);
 	}
 
 	close(x);
